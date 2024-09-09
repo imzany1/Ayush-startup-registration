e82e46612bdde2ade27e789d8c4a4d5a5e0c7d89
Hussain <sayedhusainabbashaider78600@gmail.com>
2024-09-09 23:55:10 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,15 @@
+const submitBtn = document.querySelector('.submit-btn');
+submitBtn.addEventListener('click', passwordMatch)
+function passwordMatch(e){
+    const password = document.querySelector('#exampleInputPassword1').value;
+    const cnfrmPassword = document.querySelector('#exampleInputPassword2').value;
+    console.log(password)
+    console.log(cnfrmPassword)
+    if(password == cnfrmPassword && password.length >= 8){
+        console.log("Hello"); 
+    }
+    else{
+        console.log("Bye")
+        e.preventDefault()
+    } 
+}
